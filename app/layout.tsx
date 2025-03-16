78fdc2245c766160c5015345d646ee7d4b094f24
lylares <lylares@users.noreply.github.com>
2025-03-16 20:01:22 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,10 +37,7 @@ export default function RootLayout({
                 rel="noopener noreferrer"
                 className="text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors flex items-center text-base"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
-                </svg>
-              💗 zippland/worth-calculator
+              Power by zippland/worth-calculator
               </a>
             </div>
           </div>
